79887de039c9c25834e6b5f775c21201812d7422
Manish <manishgupta@pearlorganisation.com>
2025-08-18 17:32:29 +0530

    adasd

--- src/lib/redux/actions/taskAction.ts
+++ src/lib/redux/actions/taskAction.ts
@@ -1,31 +1,3 @@
-// import { axiosInstance } from "@/util/axiosInstance";
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-// let config ={
-//     headers:{
-//         'Content-Type':'application/json'
-//     }
-// }
-// export interface Task {
-//   projectId: string;
-//   title: string;
-//   description: string;
-//   due_date: string;  
-//   status: "todo" | "in-progress" | "done"; 
-// }
-
-// export const addTask = createAsyncThunk(
-//     'create/task',async(taskData:Task, {rejectWithValue})=>{
-//        try {
-//         const { projectId } = taskData
-//         const { data } = await axiosInstance.post(`/api/v1/task/${projectId}`,taskData,config)
-//         return data
-//        } catch (error) {
-//         return rejectWithValue(error)
-//        }
-//     }
-// )
-
-
 import { axiosInstance } from "@/util/axiosInstance";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
@@ -35,8 +7,7 @@ let config = {
   },
 };
 
-// Task Interface
-export interface Task {
+ export interface Task {
   projectId: string;
   title: string;
   description: string;
@@ -44,8 +15,7 @@ export interface Task {
   status: "todo" | "in-progress" | "done";
 }
 
-// Project Interface (for update)
-export interface ProjectUpdate {
+ export interface ProjectUpdate {
   id: string;
   title?: string;
   description?: string;
